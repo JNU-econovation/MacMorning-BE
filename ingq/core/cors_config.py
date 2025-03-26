0854d53ec77a3973458edbd99c799818ddd44cc5
JeongInJae <93825184+injae-348@users.noreply.github.com>
2025-03-26 10:40:40 +0900

    chore: 초기 세팅 (#3)
    * init: uv로 프로젝트 생성
    
    - uv init 명령어로 ingq 프로젝트 생성
    
    - uv add 로 의존성 추가
      - alembic: db 마이그레이션 관리 라이브러리
      - cryptography: 암호화, 복호화 등의 라이브러리
      - fastapi:
      - pymysql: python에서 mysql 연결 라이브러리
      - python-dotenv: .env 파일로 환경 변수 관리 라이브러리
      - sqlalchemy: Python ORM
      - uvicorn: 비동기 ASGI 서버
    
    - main.py: 테스트 엔드포인트 추가
    
    * chore: .gitignore 추가
    
    * feat: DB 설정 파일 추가
    
    * feat: cors 설정 파일 추가
    
    * feat: db 설정 및 테스트 user 모델 추가
    
    * feat: lifespan, CORS 설정 및 db 연결 테스트 엔드포인트 추가
    
    * feat: Docker 환경 추가
    
    - ingq/Dockerfile: ingq BE 애플리케이션을 위한 Dockerfile 추가
    - ingq/.dockerignore: 불필요한 파일 제외하여 빌드 최적화
    - docker-compose.yml: mysql 및  ingq 애플리케이션 관리
    
    * init: uv로 ai 프로젝트 생성
    
    - uv add로 fastapi, uvicorn[standard] 설치
    
    * feat: ai 프로젝트 Docker 환경 추가
    
    - ai/Dockerfile: ingq AI 애플리케이션을 위한 Dockerfile 추가
    - ai/.dockerignore: 불필요한 파일 제외하여 빌드 최적화
    - docker-compose.yml: 포트 번호 환경변수화 및 ai 서버 설정 추가
    
    * docs: README.md 작성
    
    - 1. 개발 환경 설정
    - 2. 로컬 테스트 필요 시 docker-compose 사용

---
+++ ingq/core/cors_config.py
@@ -0,0 +1,18 @@
+from fastapi.middleware.cors import CORSMiddleware
+
+class CorsConfig:
+    def __init__(self, app):
+        
+        origins = [
+            "http://localhost:3000",
+            "https://localhost:3000"
+        ]
+        
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins = origins,
+            allow_credentials = True,
+            allow_methods = ["*"],
+            allow_headers = ["*"] 
+        )
+    
