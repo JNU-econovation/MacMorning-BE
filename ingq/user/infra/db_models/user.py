9c0ce473e65a6e30c7e63f43538c602fd45689dd
injae-348 <ant0593@naver.com>
2025-04-02 15:23:59 +0900

    feat/be: User ORM 모델 및 저장소(repository) 구현
    - user.py: ORM 기반 유저 모델
    - user_repository_impl.py: User 저장 및 이메일로 검색 기능 구현

---
+++ ingq/user/infra/db_models/user.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from sqlalchemy import String, DateTime, Enum
+from sqlalchemy.orm import Mapped, mapped_column
+from db.database import Base
+
+from user.domain.provider import Provider
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[str] = mapped_column(String(36), primary_key=True)
+    email: Mapped[str] = mapped_column(String(64), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(64), nullable=True)
+    nickname: Mapped[str] = mapped_column(String(10), nullable=False, unique=True)
+    profile_image: Mapped[str] = mapped_column(
+        String(255),
+        nullable=True,
+        server_default="https://www.gravatar.com/avatar/?d=mp",
+    )
+    username: Mapped[str] = mapped_column(String(10), nullable=False)
+    phone_number: Mapped[str] = mapped_column(String(128), nullable=False, unique=True)
+    provider: Mapped[Provider] = mapped_column(Enum(Provider), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
