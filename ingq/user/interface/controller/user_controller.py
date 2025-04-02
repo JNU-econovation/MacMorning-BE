dd1ad8400013a92d6016cda4b9ad338530181589
injae-348 <ant0593@naver.com>
2025-04-02 15:58:10 +0900

    feat/be: 자체 회원가입 엔드포인트 추가

---
+++ ingq/user/interface/controller/user_controller.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends
+from user.dto.schemas import SignUpRequest, SignUpResponse
+
+from user.application.user_service import UserService
+
+from dependency_injector.wiring import inject, Provide
+from containers import Container
+
+router = APIRouter(prefix="/v1")
+
+
+@router.post("/signup", status_code=201)
+@inject
+def sign_up(
+    user: SignUpRequest,
+    user_service: UserService = Depends(Provide[Container.user_service]),
+) -> SignUpResponse:
+    _user = user_service.register_user(user)
+    return _user
