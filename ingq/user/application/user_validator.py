0ed3a124ddacb3956d91cd35b7bf10bb1891c5f2
injae-348 <ant0593@naver.com>
2025-04-02 20:12:34 +0900

    fix/be: UserValidator 중복 회원가입 예외처리 순서 변경
    - 기존: 발생한 HTTPException을  Exception에서 잡아서 처리(500에러 반환)
    - 변경: 중복 회원가입에 대한 예외처리 순서 변경을 통해 HTTPException으로
      처리(422에러 반환)

--- ingq/user/application/user_validator.py
+++ ingq/user/application/user_validator.py
@@ -6,15 +6,18 @@ from user.domain.user import User
 class UserValidator:
     def is_user_exist(self, user: User) -> None:
         try:
-            if user is not None:
+            if user:
                 raise HTTPException(
                     status_code=422, detail="이미 존재하는 이메일입니다."
                 )
+        except HTTPException:
+            raise
         except SQLAlchemyError as e:
             raise HTTPException(
-                status_code=500, detail="데이터베이스에서 오류가 발생했습니다."
+                status_code=500,
+                detail=f"데이터베이스에서 오류가 발생했습니다.: error{e}",
             )
         except Exception as e:
             raise HTTPException(
-                status_code=500, detail="알 수 없는 오류가 발생했습니다."
+                status_code=500, detail=f"알 수 없는 오류가 발생했습니다.: error{e}"
             )
