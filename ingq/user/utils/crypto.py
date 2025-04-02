26140bd0f29bfd948932dcf9021429948949806c
injae-348 <ant0593@naver.com>
2025-04-02 15:42:33 +0900

    feat/be: 암호화 utils 추가
    - 해시 기반 비밀번호 암호화 함수 추가
    - Fernet을 통한 전화번호 암호화 함수 추가

---
+++ ingq/user/utils/crypto.py
@@ -0,0 +1,26 @@
+from cryptography.fernet import Fernet
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class Crypto:
+    def __init__(self):
+        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+        key = os.getenv("ENCRYPTION_KEY")
+        print("keykeykey: ", key)
+        self.fernet = Fernet(key)
+
+    def encrypt(self, secret):
+        return self.pwd_context.hash(secret)
+
+    def verify(self, secret, hash):
+        return self.pwd_context.verify(secret, hash)
+
+    def phone_encrypt(self, plaintext: str) -> str:
+        return self.fernet.encrypt(plaintext.encode()).decode()
+
+    def phone_decrypt(self, ciphertext: str) -> str:
+        return self.fernet.decrypt(ciphertext.encode()).decode()
