bd2c702a3f43fad7df3efb92d8bfff875654ac46
LeeJS9856 <wnstj1057@naver.com>
2025-04-03 15:47:46 +0900

    feat/ai : db 및 cors 설정 파일 추가

---
+++ ai/core/db_config.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+env_path = Path(__file__).parent / '.env'
+load_dotenv(env_path)
+
+class DbConfig:
+    
+    MYSQL_USERNAME = os.getenv("MYSQL_USERNAME")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_HOST = os.getenv("MYSQL_HOST")
+    MYSQL_PORT = os.getenv("MYSQL_PORT")
+    MYSQL_DB_NAME = os.getenv("MYSQL_DB_NAME")
+    
+    DATABASE_URL = f"mysql+pymysql://{MYSQL_USERNAME}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB_NAME}"
+    
+db_config = DbConfig()
