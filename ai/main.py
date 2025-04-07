f4f9ff6424a6fcdd82bfa5d2c7c47cef1d5bf709
LeeJS9856 <wnstj1057@naver.com>
2025-04-07 17:04:15 +0900

    feat/ai : generate_story 파라미터 변수 따로 설정가능하도록 추가

--- ai/main.py
+++ ai/main.py
@@ -14,8 +14,11 @@ def test(db:Session = Depends(get_db)):
     try:
         content = db.query(Content).first()
         if content:
-            result = generate_story(content.content, "일반적인 학생의 이야기를 쓰고싶어.")
-            return {"test":"DB 연결 성공", "content":content.content, "result":result}
+            background = "중세판타지 시대"
+            history = content.content
+            choice = "친구와 밤새 게임을 한다."
+            result = generate_story(background, history, choice)
+            return {"test":"DB 연결 성공", "result":result}
         else:
             raise HTTPException(status_code=404,detail="데이터가 없습니다.")
     except Exception as e:
