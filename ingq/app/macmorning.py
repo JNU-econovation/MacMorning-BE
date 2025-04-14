a206f1e0e0141ffcf8dcf498a11d6e27f1db62fc
injae-348 <ant0593@naver.com>
2025-04-14 17:10:50 +0900

    feat/be: Jwt 인증을 위한 AuthMiddleware 구현 및 app에 등록

--- ingq/app/macmorning.py
+++ ingq/app/macmorning.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from core.cors_config import CorsConfig
+from core.auth_middleware import AuthMiddleware
 
 from auth.interface.controller.auth_controller import router as auth_router
 from user.interface.controller.user_controller import router as user_router
@@ -27,7 +28,19 @@ def create_app() -> FastAPI:
     )
     app = FastAPI(lifespan=lifespan)
 
+    exempt_paths = [
+        "/v1/token/reissue",
+        "/v1/login",
+        "/v1/signup",
+        "/docs",
+        "/openapi.json",
+        "/redoc",
+    ]
+
     app.container = container
+    app.add_middleware(
+        AuthMiddleware, exempt_paths, jwt_token_provider=container.jwt_token_provider()
+    )
 
     CorsConfig(app=app)
 
