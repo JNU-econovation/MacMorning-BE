010007b352337838dcb49ff782febe089d62563f
injae-348 <ant0593@naver.com>
2025-04-14 16:25:26 +0900

    feat/be: Refresh Token 저장소 구현

---
+++ ingq/auth/infra/repository/redis_refresh_token_repository.py
@@ -0,0 +1,24 @@
+from db.redis_cache import redis_cache
+from auth.domain.repository.refresh_token_repository import RefreshTokenRepository
+
+
+class RedisRefreshTokenRepository(RefreshTokenRepository):
+    def __init__(self):
+        self.prefix = "refresh:"
+
+    async def save(self, user_id: str, token: str, expire: int):
+        key = f"{self.prefix}{user_id}"
+        await redis_cache.set(key, token)
+        await redis_cache.expire(key, expire)
+
+    async def get(self, user_id: str) -> str:
+        key = f"{self.prefix}{user_id}"
+        return await redis_cache.get(key)
+
+    async def delete(self, user_id: str):
+        key = f"{self.prefix}{user_id}"
+        await redis_cache.delete(key)
+
+    async def exists_by_user_id(self, user_id: str) -> bool:
+        key = f"{self.prefix}{user_id}"
+        return await redis_cache.exists(key)
