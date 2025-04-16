170b19e5bd464c1793c6b582651b7b92abb353ff
injae-348 <ant0593@naver.com>
2025-04-16 15:53:37 +0900

    refactor/be: User 도메인에서 HTTPException을 BusinessException으로 변경
    - user_service.py: UserNotFoundException, InvalidCredentialsException 적용
    - user_validator.py: UserAlreadyExistsException 적용

--- ingq/user/application/user_validator.py
+++ ingq/user/application/user_validator.py
@@ -1,23 +1,9 @@
-from fastapi import HTTPException
-from sqlalchemy.exc import SQLAlchemyError
 from user.domain.user import User
+
+from user.exception.user_exceptions import UserAlreadyExistsException
 
 
 class UserValidator:
     def is_user_exist(self, user: User) -> None:
-        try:
-            if user:
-                raise HTTPException(
-                    status_code=422, detail="이미 존재하는 이메일입니다."
-                )
-        except HTTPException:
-            raise
-        except SQLAlchemyError as e:
-            raise HTTPException(
-                status_code=500,
-                detail=f"데이터베이스에서 오류가 발생했습니다.: error{e}",
-            )
-        except Exception as e:
-            raise HTTPException(
-                status_code=500, detail=f"알 수 없는 오류가 발생했습니다.: error{e}"
-            )
+        if user:
+            raise UserAlreadyExistsException()
