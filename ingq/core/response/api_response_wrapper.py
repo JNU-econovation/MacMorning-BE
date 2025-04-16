488e9d2878583fb53ab4462f92ca25b44d9938f2
injae-348 <ant0593@naver.com>
2025-04-16 22:54:00 +0900

    refactor/be: ApiResponseWrapper의 content 타입 any에서 Any로 변경

--- ingq/core/response/api_response_wrapper.py
+++ ingq/core/response/api_response_wrapper.py
@@ -1,10 +1,11 @@
 from fastapi.responses import JSONResponse
+from typing import Any
 from core.response.success_response import SuccessResponse
 from core.exception.error_response import ErrorResponse
 
 
 class ApiResponseWrapper(JSONResponse):
-    def render(self, content: any) -> bytes:
+    def render(self, content: Any) -> bytes:
         if isinstance(content, (SuccessResponse, ErrorResponse)):
             return super().render(content.model_dump())
 
