8865c9d76b0a78ca749efff6c03680cbdd8fcdc6
injae-348 <ant0593@naver.com>
2025-04-16 23:44:29 +0900

    refactor/be: log format 상수화 및 handler가 이미 존재하는 경우 debug 로그 추가

--- ingq/utils/logging.py
+++ ingq/utils/logging.py
@@ -5,20 +5,20 @@ def get_logger(name: str, log_file: str = "ingq.log"):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
+    LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
+
     if not logger.handlers:
         # Colsole
         stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(
-            logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-        )
+        stream_handler.setFormatter(logging.Formatter(LOG_FORMAT))
 
         # File
         file_handler = logging.FileHandler(log_file, encoding="utf-8")
-        file_handler.setFormatter(
-            logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-        )
+        file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
 
         logger.addHandler(stream_handler)
         logger.addHandler(file_handler)
+    else:
+        logger.debug(f"'{name}' 핸들러가 존재합니다. 핸들러 생성을 건너뜁니다.")
 
     return logger
