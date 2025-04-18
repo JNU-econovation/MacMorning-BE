db2e8d78abc719893389ab01b8b4ab7905011180
injae-348 <ant0593@naver.com>
2025-04-18 17:47:31 +0900

    feat/be: Book ORM 모델 구현

---
+++ ingq/book/infra/db_models/book.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from sqlalchemy import Integer, String, DateTime, Boolean, JSON, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from db.database import Base
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[str] = mapped_column(
+        String(36), ForeignKey("users.id"), nullable=False
+    )
+    genre: Mapped[list[str]] = mapped_column(JSON, nullable=False)
+    gamemode: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
+    character: Mapped[dict] = mapped_column(JSON, nullable=False)
+    title: Mapped[str] = mapped_column(String(36), nullable=False)
+    background: Mapped[str] = mapped_column(String(512), nullable=False)
+    is_storage: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
