819378b3d8e9e9b246da85539ea68efe77b35736
injae-348 <ant0593@naver.com>
2025-04-18 17:50:41 +0900

    feat/be: MysqlBookRepository 구현(BookRepository 인터페이스 구현)

---
+++ ingq/book/infra/repository/mysql_book_repository.py
@@ -0,0 +1,39 @@
+from db.database import SessionLocal
+
+from book.domain.book import Book as BookVO
+from book.domain.character import Character as CharacterVO
+from book.domain.repository.book_repository import BookRepository
+
+from book.infra.db_models.book import Book
+
+
+class MysqlBookRepository(BookRepository):
+    def save(self, book: BookVO):
+        with SessionLocal() as db:
+            new_book = Book(
+                user_id=book.user_id,
+                genre=book.genre,
+                gamemode=book.gamemode,
+                character=book.character.__dict__,
+                title=book.title,
+                background=book.background,
+                is_storage=book.is_storage,
+                created_at=book.created_at,
+                updated_at=book.updated_at,
+            )
+
+            db.add(new_book)
+            db.commit()
+
+            return BookVO(
+                id=new_book.id,
+                user_id=new_book.user_id,
+                genre=new_book.genre,
+                gamemode=new_book.gamemode,
+                character=CharacterVO(**new_book.character),
+                title=new_book.title,
+                background=new_book.background,
+                is_storage=new_book.is_storage,
+                created_at=new_book.created_at,
+                updated_at=new_book.updated_at,
+            )
