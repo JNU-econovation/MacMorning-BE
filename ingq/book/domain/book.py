5b01d065df3a29bef95673473e714cec4a8dcfb1
injae-348 <ant0593@naver.com>
2025-04-18 17:31:00 +0900

    feat/be: CreateBookRequest를 Book 도메인 모델로 변환하는 클래스 메서드 추가

--- ingq/book/domain/book.py
+++ ingq/book/domain/book.py
@@ -1,7 +1,11 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 from datetime import datetime
 
 from book.domain.character import Character
+
+from book.dto.schemas import CreateBookRequest
 
 
 @dataclass
@@ -20,3 +24,26 @@ class Book:
     def __post_init__(self):
         unique_genres = list(dict.fromkeys(self.genre))
         self.genre = unique_genres[:3]
+
+    @classmethod
+    def create_book_request_to_book(
+        cls, user_id: str, request: CreateBookRequest, now: datetime
+    ) -> Book:
+        _character = Character(
+            name=request.character.name,
+            age=request.character.age,
+            gender=request.character.gender,
+            characteristic=request.character.characteristic,
+        )
+        return cls(
+            id=0,
+            user_id=user_id,
+            genre=request.genre,
+            gamemode=request.gamemode,
+            character=_character,
+            title=request.title,
+            background=request.background,
+            is_storage=request.is_storage,
+            created_at=now,
+            updated_at=now,
+        )
