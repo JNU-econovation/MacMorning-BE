b8169396ee5f3eb796625fb598f11b6abbe775bc
injae-348 <ant0593@naver.com>
2025-04-21 14:48:51 +0900

    refactor/be: 커서 직렬화/역직렬화 추상화 및 커서 기반 페이지네이션 개선
    - 애플리케이션 단에서 커서 기반 페이지네이션 수행 -> 쿼리 레벨로 이전
    - 페이지네이션 방식 변경에 따른 cursor.py 구조 리팩토링
      - Cursor 추상 클래스 도입 및 encode/decode 기능 공통화
      - CreatedAtCursor, UpdatedAtCursor, BookmarkCountCursor -> Cursor 상속 구조로 변경
      - OrderStrategy별 커서 decode 방식으로 대체
      - base64, JSON 기반 직렬화 방식 적용

--- ingq/book/infra/pagination/cursor.py
+++ ingq/book/infra/pagination/cursor.py
@@ -1,112 +1,108 @@
+import base64
+import json
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Any, Optional, Union
+from typing import Optional, Union
 
 from book.exception.book_exception import (
-    OrderStrategyAndCursorParameterMismatchException,
     UnsupportedStrategyException,
 )
 from book.infra.pagination.order_strategy import OrderStrategy
 
 
+class Cursor(ABC):
+    @abstractmethod
+    def to_dict(self) -> dict:
+        pass
+
+    @classmethod
+    def encode(cls, cursor: "Cursor") -> str:
+        cursor_data = cursor.to_dict()
+        cursor_json = json.dumps(cursor_data)
+        return base64.urlsafe_b64encode(cursor_json.encode("utf-8")).decode("utf-8")
+
+    @classmethod
+    def decode(cls, encoded_cursor: str) -> "Cursor":
+        decoded_json = base64.urlsafe_b64decode(encoded_cursor).decode("utf-8")
+        cursor_data = json.loads(decoded_json)
+        return cls.from_dict(cursor_data)
+
+    @classmethod
+    @abstractmethod
+    def from_dict(cls, cursor_data: dict) -> "Cursor":
+        pass
+
+
 @dataclass
-class CreatedAtCursor:
+class CreatedAtCursor(Cursor):
     created_at: datetime
     book_id: int
 
+    def to_dict(self) -> dict:
+        return {"created_at": self.created_at.isoformat(), "book_id": self.book_id}
+
+    @classmethod
+    def from_dict(cls, cursor_data: dict) -> "CreatedAtCursor":
+        created_at = datetime.fromisoformat(cursor_data["created_at"])
+        book_id = cursor_data["book_id"]
+        return CreatedAtCursor(created_at, book_id)
+
 
 @dataclass
-class UpdatedAtCursor:
+class UpdatedAtCursor(Cursor):
     updated_at: datetime
     book_id: int
 
+    def to_dict(self) -> dict:
+        return {"updated_at": self.updated_at.isoformat(), "book_id": self.book_id}
+
+    @classmethod
+    def from_dict(cls, cursor_data: dict) -> "UpdatedAtCursor":
+        updated_at = datetime.fromisoformat(cursor_data["updated_at"])
+        book_id = cursor_data["book_id"]
+        return UpdatedAtCursor(updated_at=updated_at, book_id=book_id)
+
 
 @dataclass
-class BookmarkCountCursor:
+class BookmarkCountCursor(Cursor):
     bookmark_count: int
     book_id: int
 
+    def to_dict(self) -> dict:
+        return {"bookmark_count": self.bookmark_count, "book_id": self.book_id}
 
-@dataclass
-class ViewCountCursor:
-    view_count: int
-    book_id: int
-
-
-def parse_cursor(cursor: str) -> dict:
-    cursor_parts = cursor.split(",")
-    cursor_dict = {
-        k: v for k, v in (part.split(":", 1) for part in cursor_parts if ":" in part)
-    }
-    return cursor_dict
+    @classmethod
+    def from_dict(cls, cursor_data: dict) -> "BookmarkCountCursor":
+        bookmark_count = cursor_data["bookmark_count"]
+        book_id = cursor_data["book_id"]
+        return BookmarkCountCursor(bookmark_count=bookmark_count, book_id=book_id)
 
 
 def validate_and_get_cursor(
-    cursor: Any, order_strategy: OrderStrategy
-) -> Optional[
-    Union[CreatedAtCursor, UpdatedAtCursor, BookmarkCountCursor, ViewCountCursor]
-]:
+    cursor: str, order_strategy: OrderStrategy
+) -> Optional[Union[CreatedAtCursor, UpdatedAtCursor, BookmarkCountCursor]]:
     """
-    커서 형식 및 타입 검증
-
-    Cursor 반환
+    커서 형식 및 타입 검증 후 Cursor 객체 반환
     """
     if not cursor:
         return None
 
-    cursor_dict = parse_cursor(cursor)
-
-    if order_strategy in [
-        OrderStrategy.CREATED_AT_DESC,
-        OrderStrategy.CREATED_AT_ASC,
-    ]:
-        if "created_at" not in cursor_dict or "book_id" not in cursor_dict:
-            raise OrderStrategyAndCursorParameterMismatchException(
-                "CreatedAt을 정렬 전략으로 선택하셨습니다. 커서 값으로 created_at과 book_id가 있는지 확인해주세요."
-            )
-        try:
-            created_at = datetime.fromisoformat(cursor_dict["created_at"])
-            book_id = int(cursor_dict["book_id"])
-            return CreatedAtCursor(created_at=created_at, book_id=book_id)
-        except (ValueError, TypeError) as err:
-            raise OrderStrategyAndCursorParameterMismatchException(
-                "ISO 형식의 시간이 맞는지, book_id가 int인지 확인해주세요."
-            ) from err
-
-    elif order_strategy in [
-        OrderStrategy.UPDATED_AT_DESC,
-        OrderStrategy.UPDATED_AT_ASC,
-    ]:
-        if "updated_at" not in cursor_dict or "book_id" not in cursor_dict:
-            raise OrderStrategyAndCursorParameterMismatchException(
-                "UpdatedAt을 정렬 전략으로 선택하셨습니다. 커서 값으로 updated_at과 book_id가 있는지 확인해주세요."
-            )
-        try:
-            updated_at = datetime.fromisoformat(cursor_dict["updated_at"])
-            book_id = int(cursor_dict["book_id"])
-            return UpdatedAtCursor(updated_at=updated_at, book_id=book_id)
-        except (ValueError, TypeError) as err:
-            raise OrderStrategyAndCursorParameterMismatchException(
-                "ISO 형식의 시간이 맞는지, book_id가 int인지 확인해주세요."
-            ) from err
-
-    # TODO: 추후 구현 예정
-    elif order_strategy in [
-        OrderStrategy.BOOKMARK_COUNT_DESC,
-        OrderStrategy.BOOKMARK_COUNT_ASC,
-    ]:
-        raise NotImplementedError(
-            f"정렬 전략 {order_strategy}에 대한 커서 처리가 아직 구현되지 않았습니다."
-        )
-
-    elif order_strategy in [
-        OrderStrategy.VIEW_COUNT_DESC,
-        OrderStrategy.VIEW_COUNT_ASC,
-    ]:
-        raise NotImplementedError(
-            f"정렬 전략 {order_strategy}에 대한 커서 처리가 아직 구현되지 않았습니다."
-        )
-
+    if (
+        order_strategy == OrderStrategy.CREATED_AT_DESC
+        or order_strategy == OrderStrategy.CREATED_AT_ASC
+    ):
+        return CreatedAtCursor.decode(cursor)
+    elif (
+        order_strategy == OrderStrategy.UPDATED_AT_DESC
+        or order_strategy == OrderStrategy.UPDATED_AT_ASC
+    ):
+        return UpdatedAtCursor.decode(cursor)
+    elif (
+        order_strategy == OrderStrategy.BOOKMARK_COUNT_DESC
+        or order_strategy == OrderStrategy.BOOKMARK_COUNT_ASC
+    ):
+        return BookmarkCountCursor.decode(cursor)
     else:
         raise UnsupportedStrategyException(
             f"{order_strategy}는 지원하지 않는 order_strategy입니다."
