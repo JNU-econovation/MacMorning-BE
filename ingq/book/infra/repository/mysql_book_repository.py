ec77364f17479022f7193bc959d7b160150f64fe
injae-348 <ant0593@naver.com>
2025-04-23 22:12:35 +0900

    refactor/be: Book ORM 객체와 Domain 객체간 매핑을 위한 staticmethod 추가

--- ingq/book/infra/repository/mysql_book_repository.py
+++ ingq/book/infra/repository/mysql_book_repository.py
@@ -3,7 +3,6 @@ from typing import Optional
 from sqlalchemy import asc, desc
 
 from book.domain.book import Book as BookVO
-from book.domain.character import Character as CharacterVO
 from book.domain.repository.book_repository import BookRepository
 from book.dto.schemas import (
     BookItem,
@@ -16,6 +15,7 @@ from book.infra.pagination.cursor import (
     validate_and_get_cursor,
 )
 from book.infra.pagination.order_strategy import OrderStrategy
+from book.utils.mapper import BookMapper
 from bookmark.infra.db_models.bookmark import Bookmark
 from db.database import SessionLocal
 from user.infra.db_models.user import User
@@ -25,36 +25,14 @@ class MysqlBookRepository(BookRepository):
     def save(self, book: BookVO) -> BookVO:
         with SessionLocal() as db:
             try:
-                new_book = Book(
-                    user_id=book.user_id,
-                    genre=book.genre,
-                    gamemode=book.gamemode,
-                    character=book.character.__dict__,
-                    title=book.title,
-                    background=book.background,
-                    is_in_progress=book.is_in_progress,
-                    created_at=book.created_at,
-                    updated_at=book.updated_at,
-                )
-
+                new_book = BookMapper.bookvo_to_book(book)
                 db.add(new_book)
                 db.commit()
             except Exception as exc:
                 db.rollback()
                 raise exc
 
-            return BookVO(
-                id=new_book.id,
-                user_id=new_book.user_id,
-                genre=new_book.genre,
-                gamemode=new_book.gamemode,
-                character=CharacterVO(**new_book.character),
-                title=new_book.title,
-                background=new_book.background,
-                is_in_progress=new_book.is_in_progress,
-                created_at=new_book.created_at,
-                updated_at=new_book.updated_at,
-            )
+            return BookMapper.book_to_bookvo(new_book)
 
     def get_all_books(
         self,
@@ -172,18 +150,7 @@ class MysqlBookRepository(BookRepository):
             if not book:
                 return None
 
-            return BookVO(
-                id=book.id,
-                user_id=book.user_id,
-                genre=book.genre,
-                gamemode=book.gamemode,
-                character=CharacterVO(**book.character),
-                title=book.title,
-                background=book.background,
-                is_in_progress=book.is_in_progress,
-                created_at=book.created_at,
-                updated_at=book.updated_at,
-            )
+            return BookMapper.book_to_bookvo(book)
 
 
 def build_query_base(db, user_id=None, progress=None):
--- ingq/book/utils/mapper.py
+++ ingq/book/utils/mapper.py
@@ -1,10 +1,12 @@
-from book.domain.book import Book
+from book.domain.book import Book as BookVO
+from book.domain.character import Character as CharacterVO
 from book.dto.schemas import CharacterResponse, CreateBookResponse
+from book.infra.db_models.book import Book
 
 
 class BookMapper:
     @staticmethod
-    def book_to_create_book_response(book: Book) -> CreateBookResponse:
+    def book_to_create_book_response(book: BookVO) -> CreateBookResponse:
         _character = CharacterResponse(
             name=book.character.name,
             age=book.character.age,
@@ -17,6 +19,35 @@ class BookMapper:
             genre=book.genre,
             gamemode=book.gamemode,
             character=_character,
+            title=book.title,
+            background=book.background,
+            is_in_progress=book.is_in_progress,
+            created_at=book.created_at,
+            updated_at=book.updated_at,
+        )
+
+    @staticmethod
+    def bookvo_to_book(book: BookVO) -> Book:
+        return Book(
+            user_id=book.user_id,
+            genre=book.genre,
+            gamemode=book.gamemode,
+            character=book.character.__dict__,
+            title=book.title,
+            background=book.background,
+            is_in_progress=book.is_in_progress,
+            created_at=book.created_at,
+            updated_at=book.updated_at,
+        )
+
+    @staticmethod
+    def book_to_bookvo(book: Book) -> BookVO:
+        return BookVO(
+            id=book.id,
+            user_id=book.user_id,
+            genre=book.genre,
+            gamemode=book.gamemode,
+            character=CharacterVO(**book.character),
             title=book.title,
             background=book.background,
             is_in_progress=book.is_in_progress,
