e958930cee6d7e3db8d763c7f12eced0b5a2709b
injae-348 <ant0593@naver.com>
2025-04-27 00:40:44 +0900

    feat/be: 북마크순 조회 엔드포인트 추가

--- ingq/book/interface/controller/book_controller.py
+++ ingq/book/interface/controller/book_controller.py
@@ -119,3 +119,26 @@ def get_bookmarked_books(
         order_strategy=order_strategy,
         cursor=cursor,
     )
+
+
+@router.get("/books/best")
+@inject
+def get_best_books(
+    request: Request,
+    limit: int = Query(4, ge=1, description="페이지당 항목 수"),
+    cursor: Optional[str] = Query(None, description="커서 값"),
+    book_service: BookService = Depends(Provide[Container.book_service]),
+    jwt_token_provider: JwtTokenProvider = Depends(
+        Provide[Container.jwt_token_provider]
+    ),
+):
+    """
+    Cursor 기반 페이지네이션을 적용해 북마크순 책 목록 조회
+
+    - limit: 페이지당 항목 수(default = 4)
+    - cursor: 이전 페이지의 마지막 항목에 대한 커서(null 가능)
+        - base64로 인코딩된 값
+    """
+    current_user = get_optional_current_user(request, jwt_token_provider)
+    user_id = current_user.id if current_user else None
+    return book_service.get_best_books(user_id, limit=limit, cursor=cursor)
