2aa42cc75617ab735af8fa4a72bb89295abb02d8
injae-348 <ant0593@naver.com>
2025-04-29 21:11:29 +0900

    feat/be: Story 생성 API 엔드포인트 추가 (일러스트 및 선택지 포함)

---
+++ ingq/story/interface/controller/story_controller.py
@@ -0,0 +1,27 @@
+from dependency_injector.wiring import Provide, inject
+from fastapi import APIRouter, Depends, Request
+from sqlalchemy.orm import Session
+
+from db.database import get_db
+from dependencies.containers import Container
+from story.application.story_service import StoryService
+from story.dto.schemas import (
+    CreateStoryWithIllustAndChoiceRequest,
+    CreateStoryWithIllustAndChoiceResponse,
+)
+
+router = APIRouter(prefix="/v1", tags=["Story Router"])
+
+
+@router.post("/story", status_code=201)
+@inject
+def create_story(
+    request: Request,
+    create_story_with_illust_and_choice_request: CreateStoryWithIllustAndChoiceRequest,
+    story_service: StoryService = Depends(Provide[Container.story_service]),
+    session: Session = Depends(get_db),
+) -> CreateStoryWithIllustAndChoiceResponse:
+    current_user = request.state.current_user
+    return story_service.create_story_with_illust_and_choice(
+        current_user.id, create_story_with_illust_and_choice_request, session
+    )
