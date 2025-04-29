34a6badc4c19405909f419cd6ec1c0eb91df7dfd
injae-348 <ant0593@naver.com>
2025-04-29 20:19:30 +0900

    feat/be: Choice ORM 모델 추가

---
+++ ingq/choice/infra/db_models/choice.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timezone
+from typing import Optional
+
+from sqlalchemy import Boolean, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from db.database import Base
+
+
+class Choice(Base):
+    __tablename__ = "choices"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    story_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("stories.id", ondelete="CASCADE"),
+        nullable=False,
+        unique=True,
+    )
+    first_choice: Mapped[str] = mapped_column(String(127), nullable=False)
+    second_choice: Mapped[str] = mapped_column(String(127), nullable=False)
+    third_choice: Mapped[Optional[str]] = mapped_column(String(255), nullable=True)
+    my_choice: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_success: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, default=datetime.now(timezone.utc)
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.now(timezone.utc),
+        onupdate=datetime.now(timezone.utc),
+    )
+
+    story = relationship("Story", back_populates="choice")
