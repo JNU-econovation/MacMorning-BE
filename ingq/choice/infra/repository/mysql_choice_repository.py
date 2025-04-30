f719c226ef8709beff99730c8872c66790e48fc6
injae-348 <ant0593@naver.com>
2025-04-30 15:42:53 +0900

    refactor/be: Choice Infra-Domain 변환 매퍼 추가

--- ingq/choice/infra/repository/mysql_choice_repository.py
+++ ingq/choice/infra/repository/mysql_choice_repository.py
@@ -2,31 +2,12 @@ from sqlalchemy.orm import Session
 
 from choice.domain.choice import Choice as ChoiceVO
 from choice.domain.repository.choice_repository import ChoiceRepository
-from choice.infra.db_models.choice import Choice
+from choice.utils.mapper import ChoiceMapper
 
 
 class MysqlChoiceRepository(ChoiceRepository):
     def save(self, choice: ChoiceVO, db: Session) -> ChoiceVO:
-        new_choice = Choice(
-            story_id=choice.story_id,
-            first_choice=choice.first_choice,
-            second_choice=choice.second_choice,
-            third_choice=choice.third_choice,
-            my_choice=choice.my_choice,
-            is_success=choice.is_success,
-            created_at=choice.created_at,
-            updated_at=choice.updated_at,
-        )
+        new_choice = ChoiceMapper.choicevo_to_choice(choice)
         db.add(new_choice)
         db.flush()
-        return ChoiceVO(
-            id=new_choice.id,
-            story_id=new_choice.story_id,
-            first_choice=new_choice.first_choice,
-            second_choice=new_choice.second_choice,
-            third_choice=new_choice.third_choice,
-            my_choice=new_choice.my_choice,
-            is_success=new_choice.is_success,
-            created_at=new_choice.created_at,
-            updated_at=new_choice.updated_at,
-        )
+        return ChoiceMapper.choice_to_choicevo(new_choice)
