ce52c4ee5ecf45ddbfd951754881b6dd9e3aaa75
injae-348 <ant0593@naver.com>
2025-04-30 14:04:44 +0900

    refactor/be: find_by_book_id_and_page_number 개별 세션으로 변경

--- ingq/story/infra/repository/mysql_story_repository.py
+++ ingq/story/infra/repository/mysql_story_repository.py
@@ -1,6 +1,8 @@
-from sqlalchemy.orm import Session
 from typing import Optional
 
+from sqlalchemy.orm import Session
+
+from db.database import SessionLocal
 from story.domain.repository.story_repository import StoryRepository
 from story.domain.story import Story as StoryVO
 from story.infra.db_models.story import Story
@@ -27,20 +29,23 @@ class MysqlStoryRepository(StoryRepository):
         )
 
     def find_by_book_id_and_page_number(
-        self, book_id: int, page_number: int, db: Session
+        self, book_id: int, page_number: int
     ) -> Optional[StoryVO]:
-        story = (
-            db.query(Story)
-            .filter(Story.book_id == book_id, Story.page_number == page_number)
-            .first()
-        )
-        if not story:
-            return None
-        return StoryVO(
-            id=story.id,
-            book_id=story.book_id,
-            page_number=story.page_number,
-            story_text=story.story_text,
-            created_at=story.created_at,
-            updated_at=story.updated_at,
-        )
+        with SessionLocal() as db:
+            story = (
+                db.query(Story)
+                .filter(Story.book_id == book_id, Story.page_number == page_number)
+                .first()
+            )
+
+            if not story:
+                return None
+
+            return StoryVO(
+                id=story.id,
+                book_id=story.book_id,
+                page_number=story.page_number,
+                story_text=story.story_text,
+                created_at=story.created_at,
+                updated_at=story.updated_at,
+            )
