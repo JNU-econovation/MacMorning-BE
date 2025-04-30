33870919c4e14165c39f0e05a499cef4b5741871
injae-348 <ant0593@naver.com>
2025-04-30 15:43:15 +0900

    Illust Infra-Domain 변환 매퍼 추가

--- ingq/illust/infra/repository/mysql_illust_repository.py
+++ ingq/illust/infra/repository/mysql_illust_repository.py
@@ -2,23 +2,12 @@ from sqlalchemy.orm import Session
 
 from illust.domain.illust import Illust as IllustVO
 from illust.domain.repository.illust_repository import IllustRepository
-from illust.infra.db_models.illust import Illust
+from illust.utils.mapper import IllustMapper
 
 
 class MysqlIllustRepository(IllustRepository):
     def save(self, illust: IllustVO, db: Session) -> IllustVO:
-        new_illust = Illust(
-            story_id=illust.story_id,
-            image_url=illust.image_url,
-            created_at=illust.created_at,
-            updated_at=illust.updated_at,
-        )
+        new_illust = IllustMapper.illustvo_to_illust(illust)
         db.add(new_illust)
         db.flush()
-        return IllustVO(
-            id=new_illust.id,
-            story_id=new_illust.story_id,
-            image_url=new_illust.image_url,
-            created_at=new_illust.created_at,
-            updated_at=new_illust.updated_at,
-        )
+        return IllustMapper.illust_to_illustvo(new_illust)
--- ingq/illust/utils/mapper.py
+++ ingq/illust/utils/mapper.py
@@ -1,12 +1,34 @@
-from illust.domain.illust import Illust
+from illust.domain.illust import Illust as IllustVO
 from illust.dto.schemas import CreateIllustResponse
+from illust.infra.db_models.illust import Illust
 
 
 class IllustMapper:
     @staticmethod
-    def to_create_illust_response(illust: Illust) -> CreateIllustResponse:
+    def to_create_illust_response(illust: IllustVO) -> CreateIllustResponse:
         return CreateIllustResponse(
             illust_id=illust.id,
+            story_id=illust.story_id,
+            image_url=illust.image_url,
+            created_at=illust.created_at,
+            updated_at=illust.updated_at,
+        )
+
+    # Infra 계층에서 사용하는 메서드
+    @staticmethod
+    def illustvo_to_illust(illust: IllustVO) -> Illust:
+        return Illust(
+            id=illust.id,
+            story_id=illust.story_id,
+            image_url=illust.image_url,
+            created_at=illust.created_at,
+            updated_at=illust.updated_at,
+        )
+
+    @staticmethod
+    def illust_to_illustvo(illust: Illust) -> IllustVO:
+        return IllustVO(
+            id=illust.id,
             story_id=illust.story_id,
             image_url=illust.image_url,
             created_at=illust.created_at,
