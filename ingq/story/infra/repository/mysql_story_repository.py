4c4da6eeca6abb80c0f79e711aca5a5258ba4be3
injae-348 <ant0593@naver.com>
2025-04-30 15:43:30 +0900

    Story Infra-Domain 변환 매퍼 추가

--- ingq/story/infra/repository/mysql_story_repository.py
+++ ingq/story/infra/repository/mysql_story_repository.py
@@ -6,27 +6,15 @@ from db.database import SessionLocal
 from story.domain.repository.story_repository import StoryRepository
 from story.domain.story import Story as StoryVO
 from story.infra.db_models.story import Story
+from story.utils.mapper import StoryMapper
 
 
 class MysqlStoryRepository(StoryRepository):
     def save(self, story: StoryVO, db: Session) -> StoryVO:
-        new_story = Story(
-            book_id=story.book_id,
-            page_number=story.page_number,
-            story_text=story.story_text,
-            created_at=story.created_at,
-            updated_at=story.updated_at,
-        )
+        new_story = StoryMapper.storyvo_to_story(story)
         db.add(new_story)
         db.flush()
-        return StoryVO(
-            id=new_story.id,
-            book_id=new_story.book_id,
-            page_number=new_story.page_number,
-            story_text=new_story.story_text,
-            created_at=new_story.created_at,
-            updated_at=new_story.updated_at,
-        )
+        return StoryMapper.story_to_storyvo(new_story)
 
     def find_by_book_id_and_page_number(
         self, book_id: int, page_number: int
@@ -41,11 +29,4 @@ class MysqlStoryRepository(StoryRepository):
             if not story:
                 return None
 
-            return StoryVO(
-                id=story.id,
-                book_id=story.book_id,
-                page_number=story.page_number,
-                story_text=story.story_text,
-                created_at=story.created_at,
-                updated_at=story.updated_at,
-            )
+            return StoryMapper.story_to_storyvo(story)
--- ingq/story/utils/mapper.py
+++ ingq/story/utils/mapper.py
@@ -1,12 +1,36 @@
-from story.domain.story import Story
+from story.domain.story import Story as StoryVO
 from story.dto.schemas import CreateStoryResponse
+from story.infra.db_models.story import Story
 
 
 class StoryMapper:
     @staticmethod
-    def to_create_story_response(story: Story) -> CreateStoryResponse:
+    def to_create_story_response(story: StoryVO) -> CreateStoryResponse:
         return CreateStoryResponse(
             story_id=story.id,
+            book_id=story.book_id,
+            page_number=story.page_number,
+            story_text=story.story_text,
+            created_at=story.created_at,
+            updated_at=story.updated_at,
+        )
+
+    # Infra 계층에서 사용하는 메서드
+    @staticmethod
+    def storyvo_to_story(story: StoryVO) -> Story:
+        return Story(
+            id=story.id,
+            book_id=story.book_id,
+            page_number=story.page_number,
+            story_text=story.story_text,
+            created_at=story.created_at,
+            updated_at=story.updated_at,
+        )
+
+    @staticmethod
+    def story_to_storyvo(story: Story) -> StoryVO:
+        return StoryVO(
+            id=story.id,
             book_id=story.book_id,
             page_number=story.page_number,
             story_text=story.story_text,
