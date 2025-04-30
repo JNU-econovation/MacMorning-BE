324c6a0261b7d496109e1343647f0af4b8cc5c23
injae-348 <ant0593@naver.com>
2025-04-30 14:05:26 +0900

    feat/be: Story 생성 시 검증 추가
    - 책이 존재하는지 검증
    - 책의 저자와 로그인한 사용자 일치 여부 검증
    - 하나의 책에 하나의 페이지만 존재하는지 검증
    - 완성된 이야기 여부 검증
    
    - 일반 모드의 경우 사용자의 선택 성공 여부는 항상 True

--- ingq/story/application/story_service.py
+++ ingq/story/application/story_service.py
@@ -1,7 +1,9 @@
 from datetime import datetime, timezone
+from typing import Optional
 
 from sqlalchemy.orm import Session
 
+from book.application.book_service import BookService
 from choice.application.choice_service import ChoiceService
 from illust.application.illust_service import IllustService
 from story.domain.repository.story_repository import StoryRepository
@@ -11,6 +13,12 @@ from story.dto.schemas import (
     CreateStoryWithIllustAndChoiceRequest,
     CreateStoryWithIllustAndChoiceResponse,
 )
+from story.exception.story_exception import (
+    DuplicatePageNumberException,
+    InvalidBookProgressException,
+    InvalidChoiceException,
+    InvalidUserAccessException,
+)
 
 
 class StoryService:
@@ -19,10 +27,12 @@ class StoryService:
         story_repository: StoryRepository,
         illust_service: IllustService,
         choice_service: ChoiceService,
+        book_service: BookService,
     ):
         self.story_repository = story_repository
         self.illust_service = illust_service
         self.choice_service = choice_service
+        self.book_service = book_service
 
     def create_story_with_illust_and_choice(
         self,
@@ -33,11 +43,29 @@ class StoryService:
     ) -> CreateStoryWithIllustAndChoiceResponse:
         now = datetime.now(timezone.utc)
         with session.begin():
+            story_request = create_story_with_illust_and_choice_request.story
+            illust_request = create_story_with_illust_and_choice_request.illust
+            choice_request = create_story_with_illust_and_choice_request.choice
+
+            book = self.book_service.get_book_by_id_or_throw(book_id)
+
+            if book.user_id != user_id:
+                raise InvalidUserAccessException()
+
+            existing_story = self.get_story_by_book_id_and_page_number(
+                book_id, story_request.page_number
+            )
+            if existing_story:
+                raise DuplicatePageNumberException()
+
+            if not book.is_in_progress:
+                raise InvalidBookProgressException()
+
             story = Story(
                 id=None,
                 book_id=book_id,
-                page_number=create_story_with_illust_and_choice_request.story.page_number,
-                story_text=create_story_with_illust_and_choice_request.story.story_text,
+                page_number=story_request.page_number,
+                story_text=story_request.story_text,
                 created_at=now,
                 updated_at=now,
             )
@@ -46,19 +74,27 @@ class StoryService:
             saved_illust = None
             saved_choice = None
 
-            if create_story_with_illust_and_choice_request.illust:
+            if illust_request:
                 saved_illust = self.illust_service.create_illust(
                     saved_story.id,
-                    create_story_with_illust_and_choice_request.illust,
+                    illust_request,
                     session,
                 )
 
-            if create_story_with_illust_and_choice_request.choice:
+            if choice_request:
+                if book.gamemode:
+                    pass
+                else:
+                    if not choice_request.is_success:
+                        raise InvalidChoiceException()
+
                 saved_choice = self.choice_service.create_choice(
                     saved_story.id,
-                    create_story_with_illust_and_choice_request.choice,
+                    choice_request,
                     session,
                 )
+            else:
+                book.set_is_in_progress_to_false()
 
         return CreateStoryWithIllustAndChoiceResponse(
             story=CreateStoryResponse(
@@ -71,4 +107,11 @@ class StoryService:
             ),
             illust=saved_illust,
             choice=saved_choice,
+        )
+
+    def get_story_by_book_id_and_page_number(
+        self, book_id: int, page_number: int
+    ) -> Optional[Story]:
+        return self.story_repository.find_by_book_id_and_page_number(
+            book_id, page_number
         )
