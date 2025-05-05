96c0bc18080a271ebeb1ca80a2c1dfe062965643
injae-348 <ant0593@naver.com>
2025-05-05 20:25:37 +0900

    feat/be: unique 파일 이름 생성 FileService 추가

---
+++ ingq/upload_image/application/file_service.py
@@ -0,0 +1,32 @@
+import mimetypes
+import os
+
+import ulid
+from fastapi import HTTPException
+
+
+class FileService:
+    @staticmethod
+    def get_content_type(filename: str) -> str:
+        content_type = mimetypes.guess_type(filename)[0]
+
+        if content_type:
+            return content_type
+
+        ext = os.path.splitext(filename)[1].lower()
+        content_type_map = {
+            ".png": "image/png",
+            ".jpg": "image/jpeg",
+            ".jpeg": "image/jpeg",
+            ".gif": "image/gif",
+            ".webp": "image/webp",
+        }
+
+        if ext not in content_type_map:
+            raise HTTPException(status_code=400, detail="Invalid file type")
+
+        return content_type_map.get(ext)
+
+    @staticmethod
+    def generate_unique_filename(original_filename: str) -> str:
+        return f"{ulid.ULID().generate()}_{original_filename}"
