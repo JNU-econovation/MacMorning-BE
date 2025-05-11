07b5d1518ae2a5b444fa7ec9092df488c1eefca6
LeeJS9856 <wnstj1057@naver.com>
2025-05-11 16:54:58 +0900

    refact/ai : baseurl 예외 추가

--- ai/client/api_client.py
+++ ai/client/api_client.py
@@ -1,3 +1,4 @@
+from typing import Any
 from dotenv import load_dotenv
 import requests
 import os
@@ -5,10 +6,12 @@ load_dotenv()
 class ApiClient :
     def __init__(self) :
         self.base_url = os.getenv("BE_BASE_URL")
+        if not self.base_url :
+            raise RuntimeError("환경 변수 BE_BASE_URL이 설정되지 않았습니다.")
 
     #be와 통신하는 기본 코드
-    def request(self, method: str, endpoint: str, authorization: str, payload: dict[str, any]=None) :
-        headers = {}
+    def request(self, method: str, endpoint: str, authorization: str | None , payload: dict[str, Any] | None = None) -> Any :
+        headers: dict[str, str] = {"Content-Type": "application/json"}
         if authorization:
             headers["Authorization"] = authorization
         
@@ -19,7 +22,6 @@ class ApiClient :
                 url = url, 
                 json = payload,
                 headers = headers)
-            print(response.request)
             response.raise_for_status()
 
             
