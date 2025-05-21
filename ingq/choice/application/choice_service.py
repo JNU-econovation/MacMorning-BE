5ab5844436faa151f449c264483ca338a3126b32
injae-348 <ant0593@naver.com>
2025-05-21 16:30:08 +0900

    feat/be: story_id 기반 ChoiceItem 조회 기능 구현

--- ingq/choice/application/choice_service.py
+++ ingq/choice/application/choice_service.py
@@ -1,10 +1,11 @@
 from datetime import datetime, timezone
+from typing import Optional
 
 from sqlalchemy.orm import Session
 
 from choice.domain.choice import Choice
 from choice.domain.repository.choice_repository import ChoiceRepository
-from choice.dto.schemas import CreateChoiceRequest, CreateChoiceResponse
+from choice.dto.schemas import ChoiceItem, CreateChoiceRequest, CreateChoiceResponse
 from choice.utils.mapper import ChoiceMapper
 
 
@@ -26,3 +27,13 @@ class ChoiceService:
         saved_choice = self.choice_repository.save(choice, db=session)
 
         return ChoiceMapper.to_create_choice_response(saved_choice)
+
+    def get_choice_item_by_story_id(
+        self, story_id: int, session: Session
+    ) -> Optional[ChoiceItem]:
+        choice = self.choice_repository.find_by_story_id(story_id, db=session)
+
+        if not choice:
+            return None
+
+        return ChoiceMapper.choicevo_to_choice_item(choice)
