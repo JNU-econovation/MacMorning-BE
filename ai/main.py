deec624be9d31d25859ce479cdda449414ac3ad4
LeeJS9856 <71966567+LeeJS9856@users.noreply.github.com>
2025-05-26 10:31:59 +0900

    feat/ai : AI 시놉시스 생성 기능 구현 (#58)
    * refact/ai: API 경로 수정 - 이야기 및 이미지 조회 엔드포인트 변경
    
    * feat/ai : 시놉시스 생성 로직 구현
    
    * fix/ai : generste_synopsys 파라미터 오류 수정
    
    * style/ai : 오탈자 수정
    
    * refact/ai : 불필요한 content 변수 삭제
    
    * fix/ai : request body에 choice는 str로 받아오도록 수정

--- ai/main.py
+++ ai/main.py
@@ -4,8 +4,41 @@ from client.book_client import get_story, get_book, get_all_story
 
 app = FastAPI()
 ai = AiService()
-@app.post("/v1/story/{book_id}")
-def story(book_id: int, choice: int = Body(None), authorization: str = Header(...)):
+
+@app.post("/v1/book/{book_id}")
+def synopsys(book_id: int, authorization: str = Header(None)):
+    try:
+        book_info = get_book(book_id, authorization)
+        if book_info :
+            genre = book_info.get("genre")
+            character = book_info.get("character")
+            background = book_info.get("background")
+            result = ai.generate_synopsys(genre, character, background)
+            return {"success": True, "data": result, "error": None}
+        else:
+            return {
+                "success": False, 
+                "data": None, 
+                "error": {
+                    "code": "BOOK001", 
+                    "status": 404, 
+                    "message": "데이터가 없습니다."
+                }
+            }
+    except Exception as e:
+        return {
+            "success": False, 
+            "data": None, 
+            "error": {
+                "code": "DB001", 
+                "status": 500, 
+                "message": "DB 연결 실패", 
+                "error": str(e)
+            }
+        }
+
+@app.post("/v1/book/{book_id}/story")
+def story(book_id: int, choice: str = Body(None), authorization: str = Header(...)):
     try:
         book_info = get_book(book_id, authorization)
         story = get_all_story(book_id, authorization)
@@ -20,12 +53,11 @@ def story(book_id: int, choice: int = Body(None), authorization: str = Header(..
     except Exception as e:
         raise HTTPException(status_code=500,detail=f"책 조회 실패: {e}") from e
 
-@app.post("/v1/story/{book_id}/image/{page_number}")
+@app.post("/v1/book/{book_id}/image/{page_number}")
 def image(book_id: int,page_number: int, authorization: str = Header(None)):
     try:
         content = get_story(book_id, page_number, authorization)
         if content:
-            content = content
             result = ai.generate_image(book_id,page_number,authorization,content)
             return {"status": "success", "result":result}
         else:
