1ed9ab774df40b8b1254672daeacd6518d471229
injae-348 <ant0593@naver.com>
2025-06-02 15:17:45 +0900

    feat/be: book id 기반 일러스트 조회 인터페이스 구현

--- ingq/illust/infra/repository/mysql_illust_repository.py
+++ ingq/illust/infra/repository/mysql_illust_repository.py
@@ -2,10 +2,12 @@ from typing import Optional
 
 from sqlalchemy.orm import Session
 
+from db.database import SessionLocal
 from illust.domain.illust import Illust as IllustVO
 from illust.domain.repository.illust_repository import IllustRepository
 from illust.infra.db_models.illust import Illust
 from illust.utils.mapper import IllustMapper
+from story.infra.db_models.story import Story
 
 
 class MysqlIllustRepository(IllustRepository):
@@ -22,3 +24,13 @@ class MysqlIllustRepository(IllustRepository):
             return None
 
         return IllustMapper.illust_to_illustvo(illust)
+
+    def find_all_by_book_id(self, book_id: int) -> list[IllustVO]:
+        with SessionLocal() as db:
+            illusts = (
+                db.query(Illust)
+                .join(Illust.story)
+                .filter(Story.book_id == book_id)
+                .all()
+            )
+        return IllustMapper.illusts_to_illustsvo(illusts)
