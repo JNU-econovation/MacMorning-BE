e91bdbcd391130ef784e9502851521b58742ad99
LeeJS9856 <wnstj1057@naver.com>
2025-07-01 10:07:15 +0900

    feat/ai: 토큰 인증 기능 추가

---
+++ ai/client/token_client.py
@@ -0,0 +1,19 @@
+from .api_client import ApiClient
+import logging
+
+api = ApiClient()
+logger = logging.getLogger(__name__)
+
+
+def token_verification(authorization):
+    endpoint = f"/v1/verification"    
+    try:
+        result = api.request("POST", endpoint, authorization)
+        if result.get("success") :
+            return True
+        else :
+            logger.debug(f"에러메시지 : {result.get('error')}")
+            return False
+    except Exception as e:
+        logger.error(f"token 인증 실패 : {e}", exc_info=True)
+        return False
